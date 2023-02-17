dbafec57935a8c0749ee64c66ff9e830734dbd39
mconcoba <mconcoba20@gmail.com>
2023-02-16 18:55:02 -0600

    feat(add): validation days for select last complet week

--- dates.py
+++ dates.py
@@ -3,7 +3,15 @@ from datetime import date, datetime, timedelta
 today = datetime.now()
 
 name_day = today.strftime('%A')
-if name_day == 'Sunday':
+if name_day == 'Thursday':
+    date_end = today - timedelta(days=6)
+elif name_day == 'Wednesday':
+    date_end = today - timedelta(days=5)
+elif name_day == 'Tuesday':
+    date_end = today - timedelta(days=4)
+elif name_day == 'Monday':
+    date_end = today - timedelta(days=3)
+elif name_day == 'Sunday':
     date_end = today - timedelta(days=2)
 elif name_day == 'Saturday':
     date_end = today - timedelta(days=1)
@@ -11,3 +19,4 @@ else:
     date_end = today
 
 date_start = date_end - timedelta(days=4)
+last_day = today - timedelta(days=1)
