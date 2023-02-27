14e4ff8327f2ea305b5f25864ed45f53e0b2d2ab
mconcoba <mconcoba20@gmail.com>
2023-02-27 00:22:22 -0600

    feat(add): lista circular

---
+++ datos/lista-circular/main.py
@@ -0,0 +1,14 @@
+from lista_circular import ListaCircualar
+
+lista = ListaCircualar()
+
+lista.agregar_final(10)
+lista.agregar_final(56)
+lista.agregar_final(78)
+lista.agregar_final(90)
+
+lista.remover_inicio()
+lista.remover_inicio()
+lista.remover_inicio()
+
+lista.recorrer()  
