75603919b0a5e8a666fe73ee6f66346a2ea6c612
mconcoba <mconcoba20@gmail.com>
2023-02-26 22:51:12 -0600

    feat(add): class of lista simple enlazada

---
+++ datos/lista-enlazada/main.py
@@ -0,0 +1,16 @@
+from lista_simple_enlazada import ListaSimpleEnlazada
+
+lista = ListaSimpleEnlazada()
+
+
+lista.agregar_ultimo(12)
+lista.agregar_ultimo(2)
+lista.agregar_ultimo(25)
+""" lista.agregar_ultimo(19)
+lista.agregar_ultimo(30) """
+
+
+lista.elimiar_inicio()
+lista.elimiar_inicio()
+
+lista.recorrido()
