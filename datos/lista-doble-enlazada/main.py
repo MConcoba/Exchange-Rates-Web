a7923d32b0d0a8e46a9b28f2baa16440e90246f6
mconcoba <mconcoba20@gmail.com>
2023-02-26 23:27:51 -0600

    feat(add): listas doblemente enalazada

---
+++ datos/lista-doble-enlazada/main.py
@@ -0,0 +1,17 @@
+from lista_doble_enlazada import LisaDobleEnlazada
+
+lista = LisaDobleEnlazada()
+
+lista.agregar_final(12)
+lista.agregar_final(56)
+lista.agregar_final(21)
+lista.agregar_final(10)
+lista.agregar_final(11)
+
+lista.elimiar_inicio()
+lista.elimiar_inicio()
+lista.elimiar_inicio()
+lista.elimiar_inicio()
+lista.elimiar_inicio()
+
+lista.recorrer_inicio()
